9c1e2dc1d4f24ce8ae54a7ea72c4b36337d94363
Rasmus Højmark Falden <rafa@itu.dk>
2022-09-16 13:48:57 +0200

    preliminary tests for the Exentions we are going to create

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -7,4 +7,35 @@ public class ExtensionsTests
     {
 
     }
+
+    [Fact]
+    public void FlattenEnumerableOfEnumerables()
+    {
+        //Arrange
+        List<List<int>> input = new List<List<int>>();
+        //input.Append(Enumerable.Empty<int>());
+        input.Add(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 10 });
+
+        input.Add(new List<int>() { 12, 34, 56, 710 });
+
+        //Act
+        //Assert
+        //Assert.Equal(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 10, 12, 34, 56, 710 }, Flatten(input));
+    }
+    [Fact]
+    public void FilterEnumerableWithEvenPredicate()
+    {
+        //Arrange
+        List<int> input = new List<int>()
+        {
+            1, 2, 3, 4, 5, 6, 7, 10, 12, 34, 56, 710
+        };
+
+        //bool Even(int i) => i % 2 == 0;
+        Predicate<int> even = (int i) => i % 2 == 0;
+
+        //Act
+        //Assert
+        //Assert.Equal(new List<int>() { 2, 4, 6, 10, 12, 34, 56, 710 }, Filter(input, even));
+    }
 }
