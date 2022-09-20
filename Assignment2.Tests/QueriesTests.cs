8342841830623073096547c8a6ab3e8a47264fd3
mbjn <mbjn@itu.dk>
2022-09-20 14:03:43 +0200

    Implemented test for find rowling

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -1,10 +1,18 @@
+using static Assignment2.Queries;
 namespace Assignment2.Tests;
 
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Given_When()
     {
+        // Arrange
+        var expected = new List<string> {"Voldemort", "Harry Potter"};
 
+        // Act
+        var rowlingList = FindMsRowling();
+
+        // Assert
+        Assert.Equal(expected, rowlingList);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,5 +1,14 @@
+using System.Linq;
 namespace Assignment2;
 
-public class Queries
+public static class Queries
 {
+    public static IEnumerable<string> FindMsRowling() {
+        WizardCollection wzCollection = WizardCollection.Create();
+        
+        return
+            from wz in wzCollection
+            where wz.Creator.Contains("Rowling", StringComparison.InvariantCultureIgnoreCase)
+            select wz.Name;
+    }
 }
